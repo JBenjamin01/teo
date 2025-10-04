05af38cc61e6e06e8f21c2a543113e4700573ee8
dmolloc1 <dmolloc@unsa.edu.pe>
2025-10-04 05:59:26 -0500

    Se termino la clase funcional sin hilos

--- lab04/java/AreaBajoLaCurva.java
+++ lab04/java/AreaBajoLaCurva.java
@@ -9,14 +9,21 @@ public class AreaBajoCurva {
         this.rango = (b - a)*1.0/num;
 
     }
-    public hallarArea(int n, int a, int b, int h){
-        Trapecio t = new Trapecio();
-        for(int i = 0;i < n; i++){
-            t.area(a, b, h);
+    public void hallarArea(int a, int b){
+        Trapecio t = new Trapecio(); 
+        int a1 = a;
+        int b1 = 0;
+        while(a1 < b){
+            a1 = hallarPunto(a1);
+            b1 = hallarPunto(a1 + this.rango);
+            this.area += t.area(a1, b1, b1 - a1);
+            a1 += this.rango
+            ;
         }
         
     }
-    public ingresarFuncion(){
 
+    public double hallarPunto(double x){
+        return 2 * Math.pow(x, 2) + 3x + 1/2.0;
     }
 }
