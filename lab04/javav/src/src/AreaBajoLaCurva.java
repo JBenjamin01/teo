832710f2f67267fa3bb8bbd436eb33684f4059c6
dmolloc1 <dmolloc@unsa.edu.pe>
2025-10-04 07:38:14 -0500

    Se configuro el main de Area bajo al curva para trabajar con hilos

--- lab04/javav/src/src/AreaBajoLaCurva.java
+++ lab04/javav/src/src/AreaBajoLaCurva.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+import javav.Trapecio;
 class Worker extends Thread {
     private double a, b, rango, area;
 
@@ -15,6 +17,7 @@ class Worker extends Thread {
 
     @Override
     public void run() {
+        System.out.println("Hilo de " + this.a + " a " + this.b + " iniciado.");
         double a1 = a;
         double b1 = 0;
         while(a1 < b){
@@ -25,38 +28,45 @@ class Worker extends Thread {
         }
     }
 
+    public double getArea() {
+        return this.area;
+    }
+
 }
 
-public class AreaBajoLaCurva {
-    private int numTrapecios;
-    private double rango;
-    private double area;
-
-    public void rango(double a, double b, int num){
-        this.rango = (b - a)*1.0/num;
-
+public class AreaBajoLaCurva implement Exception{
+    
+    public static double rango(double a, double b, int num){
+        return (b - a)*1.0/num;
     }
-    public void hallarArea(double a, double b){
-        double a1 = a;
-        double b1 = 0;
-        while(a1 < b){
-            a1 = hallarPunto(a1);
-            b1 = hallarPunto(a1 + this.rango);
-            this.area += Trapecio.area(a1, b1, b1 - a1);
-            a1 += this.rango;
-        }
-        
-    }
-
-    public double hallarPunto(double x){
-        return 2 * Math.pow(x, 2) + 3*x + 1/2.0;
+    public static double subrango(int h){
+        return this.rango / h;
     }
 
     public static void main(String[] args) {
-        
-        AreaBajoLaCurva abc = new AreaBajoLaCurva();
-        abc.rango(0, 10, 100);
-        abc.hallarArea(0, 10);
-        System.out.println("El area bajo la curva es: " + abc.area);
+        int numHilos = 5;
+        int numTrapecios = 100;
+        double a = 0;
+        double b = 10;
+        double rango = rango(a, b, numTrapecios);
+        double subRango = subrango(numHilos);
+        Worker[] hilos = new Worker[numHilos];
+
+        for (int i = 0; i < numHilos; i++) {
+            double li = a + i * subRango;
+            double ls = li + subRango;
+            hilos[i] = new Worker(li, ls, rango);
+            hilos[i].start();
+
+        }
+
+        double area = 0;
+        for (Worker w : hilos) {
+            w.join();
+            area += w.getArea();
+        }
+
+        System.out.println("El area bajo la curva es: " + area);
+
     }
 }
